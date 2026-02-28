28864022b61fc80339d26718e88cc40d08a0ecb2
I_Am_Morald <slogoff@yandex.ru>
2026-02-28 18:09:22 +0300

    Домашнее задание 21 спринт.

---
+++ app/src/main/java/com/example/playlistmaker/db/data/dao/TrackDao.kt
@@ -0,0 +1,25 @@
+package com.example.playlistmaker.db.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.playlistmaker.db.data.entity.TrackEntity
+
+@Dao
+interface TrackDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTrack(track: TrackEntity)
+
+    @Delete
+    suspend fun deleteTrack(track: TrackEntity)
+
+    @Query("SELECT * FROM favorite_tracks")
+    suspend fun getFavoriteTracks(): List<TrackEntity>
+
+    @Query("SELECT trackId FROM favorite_tracks")
+    suspend fun getTracksIdList(): List<String>
+
+}
